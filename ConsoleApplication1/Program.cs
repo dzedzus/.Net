92e313c2809b682c5ca2cb2b79111f3a42104ae5
piterpan <zarejestruj123@gmail.com>
2012-09-13 20:52:56 +0200

    ds

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Configuration;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var connectionString = ConfigurationManager.ConnectionStrings["MyDatabaseConnectionString"].ConnectionString;
+            
+            List<Stock> lista = new List<Stock>();
+            lista.Add(new Stock("dupa", 12, "chuj"));
+            lista.Add(new Stock("dupa1",122 , "chuj"));
+            
+            using ( var context = new DatabaseContext(connectionString))
+            {
+                foreach (var stock in lista)
+                {
+                    context.Stocks.Add(stock);
+                    context.SaveChanges();
+                }
+                
+
+            }
+
+        }
+        public class DatabaseContext : DbContext
+        {
+            public DatabaseContext(string connectionString)
+                : base(connectionString)
+            {
+            }
+
+
+            public DbSet<Stock> Stocks { get; set; }
+        }
+    }
+}
---
+++ ConsoleApplication1/Stock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Stock
+    {
+
+        private string stockName;
+        private float stockActualPrice;
+        private string stockBuyTime;
+        [Key]
+        public virtual string StockName { get { return stockName; } set { stockName = value; } }
+        public virtual double StockActualPrice { get { return stockActualPrice; } set { stockActualPrice = (float)value; } }
+        public virtual string StockBuyTime { get { return stockBuyTime; } set { stockBuyTime = value; } }
+
+        public Stock(string name,float price,string times)
+        {
+            stockName = name;
+            stockActualPrice = price;
+            stockBuyTime = times;
+        }
+    }
+}
